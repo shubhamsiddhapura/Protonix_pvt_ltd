c66fa5adddf2d654a9d2dc1a935d24971c062c47
Shubham Siddhapura <shubhs2844@gmail.com>
2025-04-14 10:45:49 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom"; // Import Navbar
 import Home from "./Page/Homepage";
 import Services from "./Page/ServicesMainpage";  // Ensure correct capitalization
 import Technology from "./Page/techmain";
-import About from "./page/AboutUs";
+import About from "./Page/Aboutus";
 import Contact from "./page/ContactUsMain";
 import PrivacyPolicy from "./Page/PricyPolicy";
 import Term from "./Page/Term&Condi"
