0e94a75a5d20a215da0c4c4e11fc2f457b885979
Shubham Siddhapura <shubhs2844@gmail.com>
2025-04-14 10:50:38 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./Page/Homepage";
 import Services from "./Page/ServicesMainpage";  // Ensure correct capitalization
 import Technology from "./Page/techmain";
 import About from "./Page/About";
-import Contact from "./Page/contactusmain";
+import Contact from "./Page/contactusm";
 import PrivacyPolicy from "./Page/privacypolicy";
 import Term from "./Page/Term&Condi"
 import FrontendPage from "./Page/Frontend"
